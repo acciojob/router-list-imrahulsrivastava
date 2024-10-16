35fcb8abfb3f1acad8cfe6be2ac0e3b9d269f9e1
imrahulsrivastava <imrahulsrivastava1@gmail.com>
2024-10-16 11:48:35 +0530

    submission

--- src/components/App.js
+++ src/components/App.js
@@ -11,7 +11,7 @@ const App = () => {
       <div>
         <Routes>
           <Route path="/" element={<ItemList />} />
-          <Route path="/users/:userId" element={<ItemDetail />} />
+          <Route path="/items/:userId" element={<ItemDetail />} />
         </Routes>
       </div>
     </BrowserRouter>
